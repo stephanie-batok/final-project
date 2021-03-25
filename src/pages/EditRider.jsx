22d999ce074f9f00fe9792f3a797be5f6f081282
stephaniebatok <stephaniebatok@gmail.com>
2021-03-25 12:57:54 +0200

    EditRider_Version2_Commit

--- src/pages/EditRider.jsx
+++ src/pages/EditRider.jsx
@@ -2,13 +2,16 @@ import React,{useState,useEffect} from 'react';
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import {Button,Grid,TextField,MenuItem,Container} from '@material-ui/core';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+
 
 export default function EditRider(props) {
     let { id } = useParams();
     const { register, handleSubmit} = useForm();
     const [rider,setRider] = useState("");
-    const [instructors,setInstructors]=useState([]);
-    const [horses,setHorses]=useState([]);
+    const [instructors,setInstructors]=useState("");
+    const [horses,setHorses]=useState("");
     const days =["ראשון","שני","שלישי","רביעי","חמישי","שישי"];
     const [addLesson,setAddLesson] = useState(false);
 
@@ -45,11 +48,14 @@ export default function EditRider(props) {
               (result) => {
                   setRider(result);
                   console.log('====================================');
-                  console.log(result);
+                  console.log(result.regular_lessons.length);
                   console.log('====================================');
-                  console.log(rider);
-                  console.log('====================================');
-                  console.log(rider.parents);
+
+                  if(result.regular_lessons.length===1){
+                        setAddLesson(true);
+                  }
+                  
+
               },
               (error) => {
                 alert(error);
@@ -113,7 +119,7 @@ export default function EditRider(props) {
     return (
         <Container>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Grid container justify="center" spacing={3}>
+                {rider!==""?<Grid container justify="center" spacing={3}>
                     <Grid container item justify="center" xs={6} spacing={3}>
                         <Grid item xs={12}>
                             <label>פרטים אישיים</label>
@@ -180,7 +186,7 @@ export default function EditRider(props) {
                         <Grid item xs={12}>
                             <label>פרטי הורים</label>
                         </Grid>
-                        {rider.parents.map((parent,key)=>(
+                        {rider.parents.length!==0?rider.parents.map((parent,key)=>(
                             <Grid container justify="center" spacing={3}>
                                 <Grid item xs={12}>
                                     {key===0?<label><br/>פרטי הורה ראשון</label>:<label><br/>פרטי הורה שני</label>}
@@ -189,7 +195,7 @@ export default function EditRider(props) {
                                     <TextField name={"parent_id"+key} value={parent.id} inputRef={register} label="תעודת זהות" variant="outlined" />
                                 </Grid>
                                 <Grid item>
-                                <TextField name={"parent_first_name"+key} value={parent.first_name} inputRef={register} label="שם פרטי" variant="outlined" />
+                                    <TextField name={"parent_first_name"+key} value={parent.first_name} inputRef={register} label="שם פרטי" variant="outlined" />
                                 </Grid>
                                 <Grid item>
                                     <TextField name={"parent_last_name"+key} value={parent.last_name} inputRef={register} label="שם משפחה" variant="outlined" />
@@ -205,30 +211,87 @@ export default function EditRider(props) {
                                     </TextField>
                                 </Grid>
                                 <Grid item>
-                                    <TextField name={"parent_phone"+key+1} value={parent.phone} inputRef={register} label="מספר טלפון" variant="outlined" />
+                                    <TextField name={"parent_phone"+key} value={parent.phone} inputRef={register} label="מספר טלפון" variant="outlined" />
                                 </Grid>
                                 <Grid item>
-                                    <TextField name={"parent_email"+key+1} value={parent.email} inputRef={register} label="דואר אלקטרוני" variant="outlined" />
+                                    <TextField name={"parent_email"+key} value={parent.email} inputRef={register} label="דואר אלקטרוני" variant="outlined" />
                                 </Grid>
                             </Grid>
-
-                        ))}
+                        )):
+                        <Grid container justify="center" spacing={3}>
+                            <Grid item xs={12}>
+                                <label><br/>פרטי הורה ראשון</label>
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_id0"} inputRef={register} label="תעודת זהות" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_first_name0"} inputRef={register} label="שם פרטי" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_last_name0"} inputRef={register} label="שם משפחה" variant="outlined" />
+                            </Grid>                    
+                            <Grid item>
+                                <TextField select label="בחר מגדר" variant="outlined" onChange={(e) => register({name:"parent_gender0", value: e.target.value})} style={{width:"25ch"}}>
+                                    <MenuItem value="זכר">
+                                        זכר
+                                    </MenuItem>
+                                    <MenuItem value="נקבה">
+                                        נקבה
+                                    </MenuItem>
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_phone0"} inputRef={register} label="מספר טלפון" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_email0"} inputRef={register} label="דואר אלקטרוני" variant="outlined" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                 <label><br/>פרטי הורה שני</label>
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_id1"} inputRef={register} label="תעודת זהות" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_first_name1"} inputRef={register} label="שם פרטי" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_last_name1"} inputRef={register} label="שם משפחה" variant="outlined" />
+                            </Grid>                    
+                            <Grid item>
+                                <TextField select label="בחר מגדר" variant="outlined" onChange={(e) => register({name:"parent_gender1", value: e.target.value})} style={{width:"25ch"}}>
+                                    <MenuItem value="זכר">
+                                        זכר
+                                    </MenuItem>
+                                    <MenuItem value="נקבה">
+                                        נקבה
+                                    </MenuItem>
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_phone1"} inputRef={register} label="מספר טלפון" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField name={"parent_email1"} inputRef={register} label="דואר אלקטרוני" variant="outlined" />
+                            </Grid>
+                        </Grid>}
                         <Grid item xs={12}>
-                            <label>פרטי שיעורים קבועים</label>
+                            <label><br/>פרטי שיעורים קבועים</label>
                         </Grid>
                         <Grid item>
-                            <TextField select label="בחר מדריך קבוע" variant="outlined" onChange={(e) => register({name:"instructor_id", value: e.target.value})} style={{width:"25ch"}}>
-                                {instructors.map((i) =>(
+                            <TextField select label="בחר מדריך קבוע" value={rider.instructor_id} variant="outlined" onChange={(e) => register({name:"instructor_id", value: e.target.value})} style={{width:"25ch"}}>
+                                {instructors!==""?instructors.map((i) =>(
                                 <MenuItem value={i.id}>{i.first_name+" "+i.last_name}</MenuItem>
-                                ))}
+                                )):null}
                             </TextField>
                         </Grid>
                         <Grid item>
-                            <TextField select label="בחר סוס קבוע" variant="outlined" onChange={(e) => register({name:"horse_id", value: e.target.value})} style={{width:"25ch"}}>
+                            <TextField select label="בחר סוס קבוע" value={rider.horse_id} variant="outlined" onChange={(e) => register({name:"horse_id", value: e.target.value})} style={{width:"25ch"}}>
                                 <MenuItem value={0}>ללא סוס קבוע</MenuItem>
-                                {horses.map((h) =>(
+                                {horses!==""?horses.map((h) =>(
                                 <MenuItem value={h.id}>{h.name}</MenuItem>
-                                ))}
+                                )):null}
                             </TextField>
                         </Grid>
                         {rider.regular_lessons.map((regular_lesson,key) =>(
@@ -236,11 +299,11 @@ export default function EditRider(props) {
                                 <Grid item xs={12}>
                                    {key===0?<label><br/>פרטי שיעור ראשון</label>:<label><br/>פרטי שיעור שני</label>}
                                 </Grid>
-                                {/* <Grid item>
-                                    <TextField name="price" type="number" inputRef={register} defaultValue={0} label="תעריף" variant="outlined" />
+                                <Grid item>
+                                    <TextField name={"price"+key} value={regular_lesson.price} type="number" inputRef={register} label="תעריף" variant="outlined" />
                                 </Grid>
                                 <Grid item>
-                                    <TextField select label="בחר הסדר תשלום" variant="outlined" onChange={(e) => register({name:"funding_source", value: e.target.value})} style={{width:"25ch"}}>
+                                    <TextField select label="בחר הסדר תשלום" value={regular_lesson.funding_source} variant="outlined" onChange={(e) => register({name:"funding_source"+key, value: e.target.value})} style={{width:"25ch"}}>
                                         <MenuItem value="תשלום רגיל">
                                             תשלום רגיל
                                         </MenuItem>
@@ -253,7 +316,7 @@ export default function EditRider(props) {
                                     </TextField>
                                 </Grid>
                                 <Grid item>
-                                    <TextField select label="בחר סוג שיעור" variant="outlined" onChange={(e) => register({name:"lesson_type", value: e.target.value})} style={{width:"25ch"}}>
+                                    <TextField select label="בחר סוג שיעור" value={regular_lesson.lesson_type} variant="outlined" onChange={(e) => register({name:"lesson_type"+key, value: e.target.value})} style={{width:"25ch"}}>
                                         <MenuItem value="פרטי">
                                                 פרטי
                                         </MenuItem>
@@ -263,23 +326,76 @@ export default function EditRider(props) {
                                     </TextField>
                                 </Grid>
                                 <Grid item>
-                                    <TextField select label="בחר יום קבוע" variant="outlined" onChange={(e) => register({name:"day", value: e.target.value})} style={{width:"25ch"}}>
+                                    <TextField select label="בחר יום קבוע" value={regular_lesson.day} variant="outlined" onChange={(e) => register({name:"day"+key, value: e.target.value})} style={{width:"25ch"}}>
                                         {days.map((d) =>(
                                         <MenuItem value={d}>{d}</MenuItem>
                                         ))}
                                     </TextField>
                                 </Grid>
                                 <Grid item>
-                                    <TextField name="time" style={{width:"25ch"}} variant="outlined" defaultValue="00:00" label="בחר שעה קבועה" type="time" inputRef={register}
+                                    <TextField name={"time"+key} style={{width:"25ch"}} value={regular_lesson.time} variant="outlined" label="בחר שעה קבועה" type="time" inputRef={register}
                                         inputProps={{ 
                                         step: 300, // 5 min
                                         }}
                                     />
-                                </Grid> */}
+                                </Grid>
                             </Grid>                            
                         ))}
-
-
+                        <Grid item xs={12} style={{display:(addLesson?"block":"none")}}>
+                            <Button startIcon={<AddCircleOutlineIcon />} style={{color:'green'}} onClick={()=>{setAddLesson(false)}}>
+                            &nbsp;הוספת שיעור   
+                            </Button>
+                        </Grid>
+                        <Grid container justify="center" spacing={3} style={{display:(addLesson?"none":"block")}}>
+                            <Grid item xs={12}>
+                                <label><br/>פרטי שיעור שני</label>
+                            </Grid>
+                            <Grid item>
+                                <TextField name="price1" type="number" inputRef={register} label="תעריף" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField select label="בחר הסדר תשלום" variant="outlined" onChange={(e) => register({name:"funding_source1", value: e.target.value})} style={{width:"25ch"}}>
+                                    <MenuItem value="תשלום רגיל">
+                                        תשלום רגיל
+                                    </MenuItem>
+                                    <MenuItem value="תשלום חלקי">
+                                        תשלום חלקי
+                                    </MenuItem>
+                                    <MenuItem value="תשלום ממומן">
+                                        תשלום ממומן
+                                    </MenuItem>
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField select label="בחר סוג שיעור" variant="outlined" onChange={(e) => register({name:"lesson_type1", value: e.target.value})} style={{width:"25ch"}}>
+                                    <MenuItem value="פרטי">
+                                            פרטי
+                                    </MenuItem>
+                                    <MenuItem value="קבוצתי">
+                                            קבוצתי
+                                    </MenuItem>
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField select label="בחר יום קבוע" variant="outlined" onChange={(e) => register({name:"day1", value: e.target.value})} style={{width:"25ch"}}>
+                                    {days.map((d) =>(
+                                    <MenuItem value={d}>{d}</MenuItem>
+                                    ))}
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField name="time1" style={{width:"25ch"}} variant="outlined" label="בחר שעה קבועה" type="time" inputRef={register}
+                                    inputProps={{ 
+                                    step: 300, // 5 min
+                                    }}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} style={{display:(addLesson?"none":"block")}}>
+                            <Button startIcon={<RemoveCircleOutlineIcon />} style={{color:'red'}} onClick={()=>{setAddLesson(true)}}>
+                            &nbsp;הסר שיעור   
+                            </Button>
+                        </Grid>
                         <Grid item xs={12}>
                             <Button variant="outlined"
                                     color="primary"
@@ -288,11 +404,8 @@ export default function EditRider(props) {
                             </Button>
                         </Grid>
                     </Grid>
-                </Grid>
+                </Grid>:null}
             </form>
-
-
-                        
         </Container>
     )
 }
